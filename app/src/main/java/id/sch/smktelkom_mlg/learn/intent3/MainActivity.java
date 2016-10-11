bda7d0bd0dca01908d50dd68e823cadb9030bb08
Ridjal Fathoni <fathonirosyidi@gmail.com>
2016-10-11 12:00:43 +0700

    Intent Telepon

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -18,6 +18,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
 
+        findViewById (R.id.imagetlp).setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick (View v) {
+                dialPhoneNumber ("0341712500");
+            }
+        });
+
         findViewById (R.id.imagebr).setOnClickListener (new View.OnClickListener () {
             @Override
             public void onClick (View v) {
@@ -39,6 +46,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void dialPhoneNumber (String phoneNumber) {
+        Intent intent = new Intent (Intent.ACTION_DIAL);
+        intent.setData (Uri.parse ("tel:" + phoneNumber));
+        if (intent.resolveActivity (getPackageManager ()) != null) startActivity (intent);
     }
 
     public void capturePhoto () {
