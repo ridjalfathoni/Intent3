ff2f17a4a6fdfb0e0176f031445b3ecae4a71d58
Ridjal Fathoni <fathonirosyidi@gmail.com>
2016-10-11 11:01:10 +0700

    Intent SMS

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -1,7 +1,9 @@
 package id.sch.smktelkom_mlg.learn.intent3;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate (Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
+
+        findViewById (R.id.imagesms).setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick (View v) {
+                composeSmsMessage ("Pesan dari SMK Telkom Malang");
+            }
+        });
+    }
+
+    private void composeSmsMessage (String message) {
+        Intent intent = new Intent (Intent.ACTION_SEND);
+        intent.setType ("text/plain");
+        intent.putExtra ("sms_body", message);
+        if (intent.resolveActivity (getPackageManager ()) != null) startActivity (intent);
     }
 }
