fb2bffe224b1bebd3fb16d23acc9c7ed66fe30a5
Ridjal Fathoni <fathonirosyidi@gmail.com>
2016-10-11 11:16:30 +0700

    Intent Browser

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent3/MainActivity.java
@@ -1,6 +1,7 @@
 package id.sch.smktelkom_mlg.learn.intent3;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -12,12 +13,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_main);
 
+        findViewById (R.id.imagebr).setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick (View v) {
+                openWebPage ("http://www.smktelkom-mlg.sch.id/");
+            }
+        });
+
         findViewById (R.id.imagesms).setOnClickListener (new View.OnClickListener () {
             @Override
             public void onClick (View v) {
                 composeSmsMessage ("Pesan dari SMK Telkom Malang");
             }
         });
+    }
+
+    private void openWebPage (String url) {
+        Uri webpage = Uri.parse (url);
+        Intent intent = new Intent (Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity (getPackageManager ()) != null) startActivity (intent);
     }
 
     private void composeSmsMessage (String message) {
